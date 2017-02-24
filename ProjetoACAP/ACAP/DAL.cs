4fe6f0d73970a325f7a613fdc0aa7159d2e0c8d4
Maria Clara <clarinhacoliveira@gmail.com>
2017-02-24 12:03:37 -0300

    enviando projeto PABD

---
+++ ProjetoACAP/ACAP/DAL.cs
@@ -0,0 +1,59 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACAP
+{
+    public class DAL
+    {
+        static string servidor = "127.0.0.1"; //localhost
+        static string porta = "5432"; //porta defaut
+        static string userName = "postgres"; //nome do administrador
+        static string password = "root"; //senha do administrador
+        static string banco = "ProjetoACAP"; //nome do banco de dados
+
+
+        NpgsqlConnection pgsqlConnection = null;
+        string connectionString = null;
+
+        public DAL()
+        {
+            connectionString = "Server=" + servidor + ";Port=" + porta + ";User Id=" + userName +
+                ";Password=" + password + ";Database=" + banco + ";";
+        }
+
+
+            public void InserirPessoa(string nome, string sobrenome, string email, bool associado)
+        {
+
+
+            try {
+                using (NpgsqlConnection pgsqlConnection = new NpgsqlConnection(connectionString))
+                {
+                    pgsqlConnection.Open();
+                    string cmdInserir = String.Format("Insert Into Pessoa(nome, sobrenome, email, associado) values ('{0}', '{1}', '{2}', {3})", nome, sobrenome, email,associado);
+                    using (NpgsqlCommand pgsqlcommand = new NpgsqlCommand(cmdInserir, pgsqlConnection))
+                    {
+                        pgsqlcommand.ExecuteNonQuery();
+                    }
+                    pgsqlConnection.Close();
+                }
+            }
+            catch (NpgsqlException ex)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+
+    }
+}
+
---
+++ ProjetoACAP/ACAP/Pessoa.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ACAP
+{
+    /// <summary>
+    /// Interaction logic for Pessoa.xaml
+    /// </summary>
+    public partial class Pessoa : Window
+    {
+        public Pessoa()
+        {
+            InitializeComponent();
+        }
+
+        private void BTProximo_Click(object sender, RoutedEventArgs e)
+        {
+            DAL dao = new DAL();
+            dao.InserirPessoa(TBNome.Text, TBSobrenome.Text,TBEmail.Text, (bool)CBAssociado.IsChecked);
+        }
+    }
+}
